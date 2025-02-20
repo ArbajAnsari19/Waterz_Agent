2a2ea34409b45c8ea53e1c246221cb346dd4c59b
ArbajAnsari19 <a_ansari@mt.iitr.ac.in>
2025-02-20 16:29:25 +0530

    Local changes

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { bookingAPI } from "../../api/bookingApi";
 import { GoMultiSelect } from "react-icons/go";
 import { BiSad } from 'react-icons/bi';
+import {toast} from 'react-toastify';
 import { useAppDispatch } from "../../redux/store/hook";
 import { setLoading } from "../../redux/slices/loadingSlice";
 
